ce02c39c9461d20a202fc946d1a56a8db81fc79e
Daviddor95 <gdavidexg@gmail.com>
2023-03-22 01:18:06 +0200

    Added drawer and screens

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ProductsScreen from './pages/products';
+import RecipesScreen from './pages/recipes';
+import ArticlesScreen from './pages/articles';
+import DonationsScreen from './pages/donations';
+import GroceryListsScreen from './pages/grocery_lists';
+import NotificationsScreen from './pages/notifications';
+import SettingsScreen from './pages/options';
+import StatisticsScreen from './pages/statistics';
+
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Drawer.Navigator initialRouteName="Products">
+        <Drawer.Screen name="Products" component={ProductsScreen} />
+        <Drawer.Screen name="Recipes" component={RecipesScreen} />
+        <Drawer.Screen name="Grocery lists" component={GroceryListsScreen} />
+        <Drawer.Screen name="Articles" component={ArticlesScreen} />
+        <Drawer.Screen name="Donations" component={DonationsScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        <Drawer.Screen name="Statistics" component={StatisticsScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/products.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ProductsScreen() {
+    return (
+        <View  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Products</Text>
+        </View>
+    )
+}
