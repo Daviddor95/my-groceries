61eabc2e162ef23d4973ea17317c36a0b442d596
Ella Kharakh <ella.kharakh@live.biu.ac.il>
2023-06-04 14:47:34 +0300

    updated the product list screen

---
+++ pages/components/product.js
@@ -0,0 +1,124 @@
+import * as React from 'react'
+import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import {Image ,StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+
+const Product = ({ name, expiryDate, onDelete })=>{
+    const [amount, setAmount] = useState(0);
+    //const img = require('./../../assets/tomato.jpg'); // need to be evantually the "item.image"
+    const img = require('./../../assets/product692_model0_image0_2021-12-29_11-59-09.jpg');
+    
+    const handleIncrement = () => {
+        setAmount(amount + 1);
+        //another line needs to be added for updating the database of the user
+    };
+  
+    const handleDecrement = () => {
+        if (amount > 0) {
+            //another line needs to be added for updating the database of the user
+            setAmount(amount - 1);
+        }
+    };
+  
+    const handleDelete = () => {
+        onDelete();
+        //another line needs to be added for updating the database of the user
+    };
+  
+    return (
+    <View style={styles.container}>
+        <View style={styles.item}>
+            <Image style={styles.productTinyImage} source={img}/>
+            <View style={styles.leftContainer}>
+
+                <Text style={styles.name}>{name}</Text>
+
+                <View style={styles.amountContainer}>
+
+                    <TouchableOpacity onPress={handleDecrement}>
+                        <Ionicons name="remove-circle-outline" size={24} color="black" />
+                    </TouchableOpacity>
+
+                    <Text style={styles.amount}>{amount}</Text>
+
+                    <TouchableOpacity onPress={handleIncrement}>
+                        <Ionicons name="add-circle-outline" size={24} color="black" />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={handleDelete}>
+                        <Ionicons name="trash-outline" size={24} color="black" style={styles.trashCanTinyImage}/>
+                    </TouchableOpacity>
+
+                </View>
+
+                <Text style={styles.expiryDate}>Expiration Date: {expiryDate}</Text>
+          </View>
+        </View>
+    </View>
+    );
+};
+  
+const styles = StyleSheet.create({
+    container: {
+        padding: 0,
+        backgroundColor: '#F5F5F5',
+        marginVertical: 5,
+        paddingBottom:0,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    item: {
+        padding: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 0,
+    },
+    rightContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        flex: 1,
+    },
+    leftContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginLeft: 10,
+        flex: 1,
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginBottom: 10,
+        textAlign: 'right',
+    },
+    amountContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    amount: {
+        paddingHorizontal: 10,
+        fontSize: 16,
+    },
+    expiryDate: {
+        fontSize: 14,
+        color: '#666',
+        marginTop: 3,
+        textAlign: 'right',
+        //marginBottom: 0,
+    },
+    productTinyImage: {
+        width: 87,
+        height: 87,
+        resizeMode: 'contain'
+    },
+    trashCanTinyImage: {
+        marginLeft: 10,
+        //marginLeft: 180,
+        //marginLeft: 150,
+        //marginLeft: 90,
+    },
+});
+
+export default Product;
--- pages/components/products_list.js
+++ pages/components/products_list.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import { useState } from 'react';
-import { Animated,StyleSheet, Text, View, Button, FlatList, SafeAreaView,TouchableOpacity, ScrollView} from 'react-native';
+import { Animated, Image ,ImageBackground, StyleSheet, Text, View, Button, FlatList, SafeAreaView,TouchableOpacity,Dimensions, ScrollView} from 'react-native';
 import { useNavigation, NavigationContainer } from '@react-navigation/native';
 //import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 // import { FloatingAction } from "react-native-floating-action";
 import { AntDesign } from '@expo/vector-icons'; // import icons from expo vector icons library
+import Product from './product';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const styles = StyleSheet.create({
     container: {
@@ -31,42 +33,77 @@ const styles = StyleSheet.create({
     listContainer: {
       paddingBottom: 20,
     },
+    // productTinyImage: {
+    //     width: 75,
+    //     height: 75,
+    // },
 });
 
 //const navigation = useNavigation();
-const Refrigerator= () => {
-    const [foodProducts, setFoodProducts] = useState([
-        { key: '1', name: 'Banana' },
-        { key: '2', name: 'Apple' },
-        { key: '3', name: 'Orange' },
-        { key: '4', name: 'Mango' },
-        { key: '5', name: 'Pineapple' },
-        { key: '6', name: 'Banana' },
-        { key: '7', name: 'Apple' },
-        { key: '8', name: 'Orange' },
-        { key: '9', name: 'Mango' },
-        { key: '10', name: 'Pineapple' },
-        { key: '11', name: 'Banana' },
-        { key: '12', name: 'Apple' },
-        { key: '13', name: 'Orange' },
-        { key: '14', name: 'Mango' },
-        { key: '15', name: 'Pineapple' },
+function Refrigerator() {
+    const [products, setProducts] = useState([
+        { id: '1', name: 'Apple', expiryDate: '21.04.23' },
+        { id: '2', name: 'Banana', expiryDate: '21.04.25' },
+        { id: '3', name: 'Orange', expiryDate: '21.04.28' },
+        { id: '4', name: 'Apple', expiryDate: '21.04.23' },
+        { id: '5', name: 'Banana', expiryDate: '21.04.25' },
+        { id: '6', name: 'Orange', expiryDate: '21.04.28' },
+        { id: '7', name: 'Apple', expiryDate: '21.04.23' },
+        { id: '8', name: 'Banana', expiryDate: '21.04.25' },
+        { id: '9', name: 'Orange', expiryDate: '21.04.28' },
+        { id: '10', name: 'Apple', expiryDate: '21.04.23' },
+        { id: '11', name: 'Banana', expiryDate: '21.04.25' },
+        { id: '12', name: 'Orange', expiryDate: '21.04.28' },
+        { id: '13', name: 'Apple', expiryDate: '21.04.23' },
+        { id: '14', name: 'Banana', expiryDate: '21.04.25' },
+        { id: '15', name: 'Orange', expiryDate: '21.04.28' },
+
     ]);
-    return (
-    <SafeAreaView style={styles.container}>
-        <FlatList 
-        data={foodProducts}
-        renderItem={({ item }) => ProductDetail(0, item)}
-        keyExtractor={(item) => item.key}
-        contentContainerStyle={styles.listContainer}/>
-        </SafeAreaView>
-      );
+    
+    const handleDelete = (productId) => {
+        setProducts(products.filter((item) => item.id !== productId));
+        //maybe another line needs to be added for updating the database of the user
     };
+    
+    return (
+    <View>
+        <FlatList data={products} keyExtractor={(item) => item.id}
+        renderItem={
+            ({ item }) => (
+            <Product name={item.name} expiryDate={item.expiryDate} onDelete={() => handleDelete(item.id)}/>
+        )}
+        />
+    </View>
+    );
+};
+    // const [foodProducts, setFoodProducts] = useState([
+    //     { key: '1', name: 'Banana' },
+    //     { key: '2', name: 'Apple' },
+    //     { key: '3', name: 'Orange' },
+    //     { key: '4', name: 'Mango' },
+    //     { key: '5', name: 'Pineapple' },
+    //     { key: '6', name: 'Banana' },
+    //     { key: '7', name: 'Apple' },
+    //     { key: '8', name: 'Orange' },
+    //     { key: '9', name: 'Mango' },
+    //     { key: '10', name: 'Pineapple' },
+    //     { key: '11', name: 'Banana' },
+    //     { key: '12', name: 'Apple' },
+    //     { key: '13', name: 'Orange' },
+    //     { key: '14', name: 'Mango' },
+    //     { key: '15', name: 'Pineapple' },
+    // ]);
     // return (
-    // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    //     <Text>Refrigerator!</Text>
-    // </View>
-    // );};
+    // <SafeAreaView style={styles.container}>
+    //     <FlatList 
+    //     data={foodProducts}
+    //     renderItem={({ item }) => ProductDetail(item)}
+    //     keyExtractor={(item) => item.key}
+    //     contentContainerStyle={styles.listContainer}/>
+    //     </SafeAreaView>
+    //   );};
+
+    
 
 function Freezer() {
     return (
@@ -85,21 +122,21 @@ function KitchenCabinet() {
 }
 
 
-const ProductDetail = (amount, item) => {
-    
-    return (
-    <View>
-        <Text style={styles.item}>{item.name}</Text>
-        <Text>amount is {amount}</Text>
-        <Button title="+" onPress={() => Alert.alert('plus')}/>
-        <Button title="-" onPress={() => Alert.alert('minus')}/>
-        <Button title="X" onPress={() => Alert.alert('X')}/>
-    </View>
-    // <TouchableOpacity onPress={handlePress}>
-    //     <Text style={{ color: 'black', fontSize: 17 }}>Scan</Text>
-    // </TouchableOpacity>
-    );
-};
+
+// function ProductDetail (item) {
+//     const img = require('./../../assets/tomato.jpg'); // need to be evantually the "item.image"
+//         //const [amount, setAmount] = useState(0);
+//     return (
+//     <View>
+//         <Image style={styles.productTinyImage} source={img}/>
+//         <Text style={styles.item}>{item.name}</Text>
+//         {/*<Text>amount is {amount}</Text>*/}
+//         {/* <Button title="+" onPress={()=>setAmount(amount + 1)}/>
+//         <Button title="-" onPress={()=>setAmount(amount - 1)}/>
+//         <Button title="X" onPress={()=>setAmount(0)}/> */}
+//     </View>
+//     );
+// }
 
 
 const FloatingScan = () => {
@@ -111,35 +148,51 @@ const FloatingScan = () => {
   
     return (
     <TouchableOpacity style={{
-        borderWidth: 1,
-        //borderColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'center',
+        borderWidth: 8,
+        //borderColor: 'black',
+        alignItems: 'flex-end',
+        justifyContent: 'space-evenly',
         width: 60,
         position: 'absolute',
-        top: 570,
-        right: 10,
+        top: 620,
+        right: 23,
         height: 60,
-        //backgroundColor: 'blue',
-        borderRadius: 100,
-        zIndex: 999,
+        backgroundColor: 'black',
+        borderRadius: 50,
+        //shadowColor: 'black', // IOS
+        //shadowOffset: { height: 1, width: 1 }, // IOS
+        //shadowOpacity: 1, // IOS
+        //shadowRadius: 70, //IOS
+        elevation: 10
+        //zIndex: 999,
         }}
-        onPress={handlePress}>
-            <Text style={{ color: 'black', fontSize: 17 }}>Scan</Text>
+        onPressIn={handlePress}>
+            <ImageBackground 
+            source={require('./../../assets/barcode-scan-custom5.png')} 
+            style={{top: 5,right: 2, width: '80%', height: '80%',backgroundColor: 'black', alignItems: 'flex-start',}}
+            />
+            <Text style={{ color: 'black', fontSize: 0 }}>Scan</Text>
     </TouchableOpacity>);
 };
-//const styles = StyleSheet.create({floatingScanButton: {position: 'absolute',bottom: 20,right: 20,},});
   
 const Tab = createMaterialTopTabNavigator();
 export default function ProductsListScreen() {
     const scanning = {text: "Scan"};
     //icon: require("./images/ic_accessibility_white.png"),
     return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1,width: Dimensions.get('window').width }}>
             <Tab.Navigator>
-                <Tab.Screen name="Kitchen Cabinet" component={KitchenCabinet}/>
+                <Tab.Screen name="cupboard" component={KitchenCabinet}/>
                 <Tab.Screen name="Refrigerator" component={Refrigerator}/>
-                <Tab.Screen name="Freezer" component={Freezer}/>
+                <Tab.Screen 
+                options={{
+                    // tabBarLabel: 'Freezer',
+                    // //tabBarLabelStyle: {width:40, fontSize:12},
+                    // tabBarItemStyle: { width: 50 }, 
+                    // //tabBarIconStyle: { marginRight: '60%', width: 60 },
+                    // //tabBarIndicatorStyle: { width: 50 }
+                }}
+                name="Freezer" component={Freezer}/>
                 {/* <FloatingScan actions={scanning} onPressItem={name => {console.log(`selected button: ${name}`);}}/> */}
             </Tab.Navigator>
             <FloatingScan />
