abb6c777a834497f6c491d26164d4c2884a26847
Ella Kharakh <ella.kharakh@live.biu.ac.il>
2023-07-29 13:37:27 +0300

    changing name of varible

--- pages/screens/recipes/recipes.js
+++ pages/screens/recipes/recipes.js
@@ -57,7 +57,7 @@ export default function RecipesScreen() {
         {console.error("Error:", error);}).finally(() => {
             setIsLoading(false);
         });
-    }
+    } 
 
     return (
     <View>
